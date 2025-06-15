b43677ab7b30ab22eeedddb58287c17a2aedde60
Try Chen <trychen@aten.com.tw>
2025-06-15 12:01:53 +0800

    Remove unused files and components: delete obsolete HTML, TypeScript definitions, routes, and API-related files

--- frontend/src/routes/_layout/feedback.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_layout/feedback')({
-  component: () => <div>Hello /_layout/feedback!</div>
-})
--- frontend/src/routes/_layout/help.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_layout/help')({
-  component: () => <div>Hello /_layout/help!</div>
-})
